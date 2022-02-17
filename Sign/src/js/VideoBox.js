254c956238197499ecd5928fb5f14137ea9274a4
Jayden <43273809+jpereira99@users.noreply.github.com>
2022-02-17 15:58:28 -0500

    Fix webcam

--- Sign/src/js/VideoBox.js
+++ Sign/src/js/VideoBox.js
@@ -1,37 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Webcam from 'react-webcam';
 
-//import logo from '../../whiteLogo.png'
-
-/*
- <script>
-                var video = document.querySelector("#videoElement");
-
-                if (navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({ video: true })
-                    .then(function (stream) {
-                    video.srcObject = stream;
-                    })
-                    .catch(function (err0r) {
-                    console.log("Something went wrong!");
-                    })
-                };
-            </script>
-*/
-
-
-export default function VideoBox(){
-    return(
-        <div >
-            <div id="container">
-	            <video autoplay="true" id="videoElement">
-
-	            </video>
-                <button id="btn">Start</button>
-            </div>
-           
-            
-        </div>
-        
-      
-    )
+export default function VideoBox() {
+	return (
+		<div>
+			<div id='container'>
+				<Webcam />
+				<button id='btn'>Start</button>
+			</div>
+		</div>
+	);
 }
